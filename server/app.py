020f45e3f10128ccab25ef013c309f2a4bca779c
victor <victormuhoro7@gmail.com>
2024-04-02 23:24:29 +0300

    Secnd test

--- server/app.py
+++ server/app.py
@@ -7,5 +7,11 @@ app = Flask(__name__)
 def index():
     return '<h1>Python Operations with Flask Routing and Views</h1>'
 
+@app.route('/print/<string:string>')
+
+def print_string(string):
+    print(string)
+    return string
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
